7e40c0a1ca399daec93b6685850e35cd48883e20
VicOnt22 <github@selectfix.com>
2023-03-28 14:34:31 -0400

    added cayuga language

--- docroot/themes/custom/grasacgks/js/scripts.js
+++ docroot/themes/custom/grasacgks/js/scripts.js
@@ -7,6 +7,10 @@
   Drupal.behaviors.grasacgks = {
     attach() {
       // grasacgks JavaScript behaviors goes here.
+
+      //make search input in headr shorter
+      $("input:first").attr('size', '20');
+
     },
   };
 })(window.jQuery, window._, window.Drupal);
